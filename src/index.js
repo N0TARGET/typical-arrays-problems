e52c8aa92d0b70c742182d4dd65d5038ad87d23d
APapok <a.papok@godeltech.com>
2020-03-10 16:26:44 +0300

    feat: implement methods (min, max, avg)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-exports.min = function min (array) {
-    if (array === undefined || array === null || array.length === 0)  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array === null || array.length === 0) return 0;
     return Math.min(...array);
 };
 
-exports.max = function max (array) {
-    if (array === undefined || array === null || array.length === 0)  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array === null || array.length === 0) return 0;
     return Math.max(...array);
 };
 
-exports.avg = function avg (array) {
-    if (array === undefined || array === null || array.length === 0)  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array === null || array.length === 0) return 0;
     const sum = array.reduce((x, y) => x + y);
     return sum / array.length;
 };
